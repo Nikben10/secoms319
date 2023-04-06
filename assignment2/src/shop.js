75038ffb4a60db5f1cbbbad8d3ae41fc64dd4294
kyle0324 <kyle0324@hotmail.com>
2023-04-06 15:12:43 -0500

    shop stuff

--- assignment2/src/shop.js
+++ assignment2/src/shop.js
@@ -1,8 +1,66 @@
 import React, { useState, useEffect } from "react";
 
+
+
+
 const Shop = () => {
+    const [cart, setCart] = useState([]);
+    const [cartTotal, setCartTotal] = useState(0);
+    //need to check tags
+    const listItems = items.map((el) => (
+        <div key={el.id}>
+            <img class=
+            "img-fluid" src={el.image} />
+            {el.title}
+            {el.category}
+            {el.price}
+            <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
+            <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+        </div>
+        ));
+    
+    useEffect(() => {
+        total();
+        }, [cart]);
+    
+    const total = () => {
+        let totalVal = 0;
+        for (let i = 0; i < cart.length; i++) {
+        totalVal += cart[i].price;
+        }
+        setCartTotal(totalVal);
+        };
+
+    const addToCart = (el) => {
+        setCart([...cart, el]);
+        };
+
+    const removeFromCart = (el) => {
+        let hardCopy = [...cart];
+        hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
+        setCart(hardCopy);
+        };
+
+    const cartItems = cart.map((el) => (
+        <div key={el.id}>
+            <img class=
+            "img-fluid" src={el.image} width={30} />
+            {el.title}
+            ${el.price}
+        </div>
+        ));
+
+
+
+
+
     return (
-    <div> Hello </div>
+    <div>
+        <div> {listItems} </div>
+        <div>items in cart: </div>
+        <div>{cartItems}</div>
+        <div>Order total to pay :{cartTotal}</div>
+    </div>
     );
 }
 
