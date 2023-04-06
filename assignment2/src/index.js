8ef0c6e869dc7b8098d91b8fe296cbde2dd36b82
bjn1 <benjaminniklasen@yahoo.com>
2023-04-06 16:54:51 -0500

    Added boostrap

--- assignment2/src/index.js
+++ assignment2/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Shop from "./shop";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- assignment2/src/shop.js
+++ assignment2/src/shop.js
@@ -8,15 +8,38 @@ const Shop = () => {
     const [cartTotal, setCartTotal] = useState(0);
     //need to check tags again
     const listItems = items.map((el) => (
-        <div key={el.id}>
-            <img className="img-fluid" src={require("./images/" + el.imageName)} />
-            {el.name}
-            {el.shortDescription}
-            {el.price}
-            <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
-            <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+        // <div key={el.id}>
+        //     <img className="img-fluid" src={require("./images/" + el.imageName)} />
+        //     {el.name}
+        //     {el.shortDescription}
+        //     {el.price}
+        //     <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
+        //     <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+        // </div>
+        <div className="row border-top border-bottom" key={el.id}>
+            <div className="row main align-items-center">
+                <div className="col-2">
+                    <img className="img-fluid" src={require("./images/" + el.imageName)} />
+                </div>
+                <div className="col">
+                    <div className="row text-muted">{el.name}</div>
+                    <div className="row">{el.shortDescription}</div>
+                </div>
+                <div className="col">
+                    <button type="button" variant="light" onClick={() => removeFromCart(el)} > - </button>{" "}
+                    <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+                </div>
+                <div className="col">
+                    ${el.price} <span className="close">&#10005;</span>{howManyofThis(el.id)}
+                </div>
+            </div>
         </div>
         ));
+
+    function howManyofThis(id) {
+        let hmot = cart.filter((cartItem) => cartItem.id === id);
+        return hmot.length;
+        }
     
     useEffect(() => {
         total();
@@ -53,11 +76,40 @@ const Shop = () => {
 
 
     return (
+    // <div>
+    //     <div> {listItems} </div>
+    //     <div>items in cart: </div>
+    //     <div>{cartItems}</div>
+    //     <div>Order total to pay :{cartTotal}</div>
+    // </div>
     <div>
-        <div> {listItems} </div>
-        <div>items in cart: </div>
-        <div>{cartItems}</div>
-        <div>Order total to pay :{cartTotal}</div>
+        Computer Store
+        <div className="card">
+            <div className="row">
+            {/* HERE, IT IS THE SHOPING CART */}
+                <div className="col-md-8 cart">
+                    <div className="title">
+                        <div className="row">
+                            <div className="col">
+                                <h4>
+                                    <b>Your Shopping Cart</b>
+                                </h4>
+                            </div>
+                            <div className="col align-self-center text-right text-muted">
+                                Products selected {cart.length}
+                            </div>
+                        </div>
+                    </div>
+                    <div>{listItems}</div>
+                </div>
+                <div className ="float-end">
+                    <p className ="mb-0 me-5 d-flex align-items-center">
+                        <span className ="small text-muted me-2">Order total:</span>
+                        <span className ="lead fw-normal">${cartTotal}</span>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
     );
 }
