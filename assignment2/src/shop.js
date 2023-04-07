31ff43d1c648ba1aeb19a8fb08c20f6afff63546
bjn1 <benjaminniklasen@yahoo.com>
2023-04-07 17:51:38 -0500

    Confirmation view and cart rework

--- assignment2/src/shop.js
+++ assignment2/src/shop.js
@@ -5,6 +5,12 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
     const [cartTotal, setCartTotal] = useState(0);
     const [page, setPage] = useState(0);            // 0 : Browse, 1 : Cart, 2 : Confirmation
+
+    let userInfo = {
+        "fullName" : "Bill Fargot",
+        "cardNumber" : "1234567890123456",
+        "city" : "West Taiwan"
+    }
     //need to check tags again
 
     const [searchString, setSearchString] = useState("");
@@ -15,7 +21,7 @@ const Shop = () => {
                     item.shortDescription.toUpperCase().includes(searchString.toUpperCase());
         });
     }
-
+    // console.log(itemsCopy);
     const listItems = itemsCopy.map((el) => (
         <div className="row border-top border-bottom" key={el.id}>
             <div className="row main align-items-center">
@@ -31,15 +37,19 @@ const Shop = () => {
                     <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
                 </div>
                 <div className="col">
-                    ${el.price} <span className="close">&#10005;</span>{howManyofThis(el.id)}
+                    ${el.price} <span className="close">&#10005;</span>{howManyofThis(el)}
                 </div>
             </div>
         </div>
         ));
 
-    function howManyofThis(id) {
-        let hmot = cart.filter((cartItem) => cartItem.id === id);
-        return hmot.length;
+    function howManyofThis(el) {
+        // let hmot = cart.filter((cartItem) => cartItem.id === id);
+        if (cart.includes(el)) {
+            return el.quantity;
+        } else {
+            return 0;
+        }
     }
     
     useEffect(() => {
@@ -49,36 +59,56 @@ const Shop = () => {
     const total = () => {
         let totalVal = 0.00;
         for (let i = 0; i < cart.length; i++) {
-            totalVal += Number(cart[i].price);
+            totalVal += Number(cart[i].price) * cart[i].quantity;
         }
         setCartTotal(totalVal.toFixed(2));
     };
 
     const addToCart = (el) => {
-        setCart([...cart, el]);
+        let hardCopy = [...cart];
+        if (hardCopy.includes(el)) {
+            el.quantity += 1;
+        } else {
+            el["quantity"] = 1;
+            hardCopy.push(el);
+        }
+        setCart(hardCopy);
     };
 
     const removeFromCart = (el) => {
         let hardCopy = [...cart];
-        let foundFirst = false;
-        hardCopy = hardCopy.filter((cartItem) => {
-            if (cartItem.id !== el.id || foundFirst) {
-                return true;
-            } else {
-                foundFirst = true;
-                return false;
+        if (hardCopy.includes(el)) {
+            el.quantity -= 1;
+            if (el.quantity == 0) {
+                hardCopy.splice(hardCopy.indexOf(el), 1);
             }
-        });
+        }
         setCart(hardCopy);
     };
 
-    // const cartItems = cart.map((el) => (
-    //     <div key={el.id}>
-    //         <img class="img-fluid" src={require("./images/" + el.imageName)} width={30} />
-    //         {el.name}
-    //         ${el.price}
-    //     </div>
-    // ));
+    function totalProducts() {
+        let total = 0;
+        for (let i of cart) {
+            total += i.quantity;
+        }
+        return total;
+    }
+
+    const cartItems = cart.map((el) => (
+        <div className="row border-top border-bottom" key={el.id}>
+            <div className="row main align-items-center">
+                <div className="col-2">
+                    <img className="img-fluid" src={require("./images/" + el.imageName)} />
+                </div>
+                <div className="col">
+                    <div className="row text-muted">{el.name}</div>
+                </div>
+                <div className="col">
+                    ${el.price} <span className="close">&#10005;</span> {el.quantity} = {(el.price * el.quantity).toFixed(2)}
+                </div>
+            </div>
+        </div>
+    ));
 
     if (page == 0) {
         return (
@@ -97,7 +127,7 @@ const Shop = () => {
                                     </input>
                                 </div>
                                 <div className="col align-self-center text-right text-muted">
-                                    Products selected {cart.length}
+                                    Products selected {totalProducts()}
                                 </div>
                             </div>
                         </div>
@@ -116,15 +146,43 @@ const Shop = () => {
         );
     } else if (page == 1) {
         return (
-            <p>
-                Cart Page
-            </p>
+            <div>
+                <p>
+                    Cart Page
+                </p>
+                <button type="button" variant="light" onClick={() => setPage(2)}> Buy </button>
+            </div>
         );
     } else {
         return (
-            <p>
-                Confirmation Page
-            </p>
+        <div>
+            Order Confirmation
+            <div className="card">
+                <div className="row">
+                    <div className="col-md-8 cart">
+                        <div className="title">
+                            {/* <div className="row"> */}
+                                <div className="col">
+                                    Thank you {userInfo.fullName} for your order using the card ending in {userInfo.cardNumber.slice(-4)}!<br />
+                                    We'll get that shipped to {userInfo.city} right away!
+                                </div>
+                                <div className="col align-self-center text-right text-muted">
+                                    Products Ordered: {cart.length}
+                                </div>
+                            {/* </div> */}
+                        </div>
+                        <div>{cartItems}</div>
+                    </div>
+                    <div className ="float-end">
+                        <p className ="mb-0 me-5 d-flex align-items-center">
+                            <span className ="small text-muted me-2">Order total:</span>
+                            <span className ="lead fw-normal">${cartTotal}</span> 
+                            <button type="button" variant="light" onClick={() => {setCart([]); setPage(0)}}> Keep Browsing </button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         );
     }
 }
