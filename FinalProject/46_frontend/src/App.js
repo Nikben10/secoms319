dad7ad2eeb9211597fdb9f09c5030ecbd3c7b9f8
kyle0324 <kyle0324@hotmail.com>
2023-05-04 17:11:49 -0500

    pictures fixed

--- FinalProject/46_frontend/src/App.js
+++ FinalProject/46_frontend/src/App.js
@@ -39,8 +39,8 @@ function App() {
 
   if (searchString.length > 0) {
     itemsCopy = product.filter((item) => {
-        return product.name.toUpperCase().includes(searchString.toUpperCase()) ||
-                product.shortDescription.toUpperCase().includes(searchString.toUpperCase());
+        return item.name.toUpperCase().includes(searchString.toUpperCase()) ||
+                item.shortDescription.toUpperCase().includes(searchString.toUpperCase());
     });
   }
 
@@ -48,7 +48,7 @@ function App() {
     <div className="row border-top border-bottom" key={el.id}>
         <div className="row main align-items-center">
             <div className="col-2">
-                <img className="img-fluid" src={el.imageName} alt={el.alt}/>
+                <img className="img-fluid" src={"http://127.0.0.1:4000/images/" + el.imageName} alt={el.alt}/>
             </div>
             <div className="col">
                 <div className="row text-muted">{el.name}</div>
@@ -119,7 +119,7 @@ function App() {
     <div className="row border-top border-bottom" key={el.id}>
         <div className="row main align-items-center">
             <div className="col-2">
-                <img className="img-fluid" src={el.imageName} />
+                <img className="img-fluid" src={"http://127.0.0.1:4000/images/" + el.imageName} />
             </div>
             <div className="col">
                 <div className="row text-muted">{el.name}</div>
@@ -138,7 +138,7 @@ const checkoutedItems = cart.map((el) => (
   <div className="row border-top border-bottom" key={el.id}>
       <div className="row main align-items-center">
           <div className="col-2">
-              <img className="img-fluid" src={el.imageName} />
+              <img className="img-fluid" src={"http://127.0.0.1:4000/images/" + el.imageName} />
           </div>
           <div className="col">
               <div className="row text-muted">{el.name}</div>
